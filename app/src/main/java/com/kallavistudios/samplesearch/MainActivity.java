ad0ad3dc3804ae73b76e807b98ba13340449535f
Emre Dirican <emre.dirican@yemeksepeti.com>
2015-12-28 17:08:50 +0200

    Optimize memory usage
    Read the suggestion files and cache only the words that are result of the query
    instead of reading and caching all of them at the start.

--- app/src/main/java/com/kallavistudios/samplesearch/MainActivity.java
+++ app/src/main/java/com/kallavistudios/samplesearch/MainActivity.java
@@ -14,7 +14,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import com.kallavistudios.samplesearch.data.DataManager;
 import com.kallavistudios.samplesearch.di.component.ActivityComponent;
 import com.kallavistudios.samplesearch.di.module.ActivityModule;
@@ -23,160 +24,161 @@ import com.kallavistudios.samplesearch.rest.response.SearchResult;
 import com.kallavistudios.samplesearch.rest.response.model.SearchResultItem;
 import com.kallavistudios.samplesearch.ui.adapter.SearchResultAdapter;
 import com.squareup.otto.Bus;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 import javax.inject.Inject;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int RESULT_LIMIT = 100;
-    @Bind(R.id.sv_main)
-    SearchView searchView;
-    @Bind(R.id.lv_main)
-    RecyclerView listView;
-    @Bind(R.id.lv_suggestions)
-    ListView suggestionsListView;
-    @Bind(R.id.layout_main_container)
-    RelativeLayout mainViewContainer;
+  private static final int RESULT_LIMIT = 100;
+  @Bind(R.id.sv_main) SearchView searchView;
+  @Bind(R.id.lv_main) RecyclerView listView;
+  @Bind(R.id.lv_suggestions) ListView suggestionsListView;
+  @Bind(R.id.layout_main_container) RelativeLayout mainViewContainer;
 
-    @Inject
-    SearchService searchService;
-    @Inject
-    Activity activityContext;
-    @Inject
-    Bus bus;
-    @Inject
-    DataManager dataManager;
+  @Inject SearchService searchService;
+  @Inject Activity activityContext;
+  @Inject Bus bus;
+  @Inject DataManager dataManager;
 
-    private SearchResultAdapter adapter;
-    private ArrayAdapter<String> suggestionsAdapter;
+  private SearchResultAdapter adapter;
+  private ArrayAdapter<String> suggestionsAdapter;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        bus.register(this);
-    }
+  @Override
+  protected void onStart() {
+    super.onStart();
+    bus.register(this);
+  }
 
-    @Override
-    protected void onStop() {
-        bus.unregister(this);
-        super.onStop();
-    }
+  @Override
+  protected void onStop() {
+    bus.unregister(this);
+    super.onStop();
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ActivityComponent objectGraph = ((SampleSearchApplication) getApplication()).getObjectGraph().plus(new ActivityModule(this));
-        objectGraph.inject(this);
-        ButterKnife.bind(this);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ActivityComponent objectGraph = ((SampleSearchApplication) getApplication()).getObjectGraph()
+        .plus(new ActivityModule(this));
+    objectGraph.inject(this);
+    ButterKnife.bind(this);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        initSearchView();
-        initSuggestionsList();
-        initSearchResultsList();
+    initSearchView();
+    initSuggestionsList();
+    initSearchResultsList();
+  }
 
-    }
+  private void initSearchResultsList() {
+    listView.setLayoutManager(new LinearLayoutManager(this));
+    adapter = new SearchResultAdapter(Collections.<SearchResultItem>emptyList());
+    listView.setAdapter(adapter);
+  }
 
-    private void initSearchResultsList() {
-        listView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new SearchResultAdapter(Collections.<SearchResultItem>emptyList());
-        listView.setAdapter(adapter);
-    }
-
-    private void initSuggestionsList() {
-        suggestionsAdapter = new ArrayAdapter<>(activityContext, R.layout.item_suggestion_result, new ArrayList<String>());
-        suggestionsListView.setAdapter(suggestionsAdapter);
-        suggestionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                search(suggestionsAdapter.getItem(position));
-                suggestionsAdapter.clear();
-            }
-        });
-    }
-
-    private void initSearchView() {
-        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView.setSubmitButtonEnabled(true);
-        searchView.setQueryRefinementEnabled(true);
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                onSearchViewSubmit(query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String query) {
-                return onSearchViewQueryTextChange(query);
-            }
-        });
-    }
-
-    private boolean onSearchViewQueryTextChange(String query) {
-        adapter.setItems(Collections.<SearchResultItem>emptyList());
-        if (query == null || query.length() < 2) {
-            return false;
-        }
+  private void initSuggestionsList() {
+    suggestionsAdapter = new ArrayAdapter<>(activityContext, R.layout.item_suggestion_result,
+        new ArrayList<String>());
+    suggestionsListView.setAdapter(suggestionsAdapter);
+    suggestionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        search(suggestionsAdapter.getItem(position));
         suggestionsAdapter.clear();
-        List<String> result = new ArrayList<>();
-        List<String> suggestions = dataManager.getSuggestions();
-        for (int i = 0, size = suggestions.size(); i < size; i++) {
-            String suggestion = suggestions.get(i).replace("'", "");
-            if (suggestion.startsWith(query)) {
-                result.add(suggestions.get(i));
-                if(result.size() >= RESULT_LIMIT){
-                    break;
-                }
-            }
+      }
+    });
+  }
+
+  private void initSearchView() {
+    SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+    searchView.setSubmitButtonEnabled(true);
+    searchView.setQueryRefinementEnabled(true);
+    searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+    searchView.setIconifiedByDefault(false);
+    searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+      @Override
+      public boolean onQueryTextSubmit(String query) {
+        onSearchViewSubmit(query);
+        return true;
+      }
+
+      @Override
+      public boolean onQueryTextChange(String query) {
+        return onSearchViewQueryTextChange(query);
+      }
+    });
+  }
+
+  private boolean onSearchViewQueryTextChange(String query) {
+    adapter.setItems(Collections.<SearchResultItem>emptyList());
+    if (query == null || query.length() < 2) {
+      return false;
+    }
+    suggestionsAdapter.clear();
+    //List<String> result = new ArrayList<>();
+    //List<String> suggestions = dataManager.getSuggestions();
+    //for (int i = 0, size = suggestions.size(); i < size; i++) {
+    //    String suggestion = suggestions.get(i).replace("'", "");
+    //    if (suggestion.startsWith(query)) {
+    //        result.add(suggestions.get(i));
+    //        if(result.size() >= RESULT_LIMIT){
+    //            break;
+    //        }
+    //    }
+    //}
+    //suggestionsAdapter.addAll(result);
+    dataManager.getSuggestedWords(query).subscribe(new Action1<List<String>>() {
+      @Override
+      public void call(List<String> strings) {
+        List<String> result = strings;
+        Collections.sort(result);
+        if(strings.size() > RESULT_LIMIT){
+          result = strings.subList(0, RESULT_LIMIT);
         }
         suggestionsAdapter.addAll(result);
-        return true;
-    }
+      }
+    });
+    return true;
+  }
 
-    private void onSearchViewSubmit(String query) {
-        adapter.setItems(Collections.<SearchResultItem>emptyList());
-        suggestionsAdapter.clear();
-        search(query);
-    }
+  private void onSearchViewSubmit(String query) {
+    adapter.setItems(Collections.<SearchResultItem>emptyList());
+    suggestionsAdapter.clear();
+    search(query);
+  }
 
+  private void search(String keyword) {
+    searchService.search(getString(R.string.apikey), getString(R.string.seId),
+        "items(title,link,snippet)", false, keyword)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io())
+        .subscribe(new Subscriber<SearchResult>() {
+          @Override
+          public void onCompleted() {
+          }
 
-    private void search(String keyword) {
-        searchService.search(getString(R.string.apikey), getString(R.string.seId), "items(title,link,snippet)", false, keyword)
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe(new Subscriber<SearchResult>() {
-                    @Override
-                    public void onCompleted() {
-                    }
+          @Override
+          public void onError(Throwable e) {
+            Timber.d(e.getMessage());
+          }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Timber.d(e.getMessage());
-                    }
-
-                    @Override
-                    public void onNext(SearchResult searchResult) {
-                        Timber.d(searchResult.toString());
-                        if (searchResult.getItems().isEmpty()) {
-                            Toast.makeText(activityContext, getString(R.string.no_result_found), Toast.LENGTH_SHORT).show();
-                        }
-                        adapter.setItems(searchResult.getItems());
-                    }
-                });
-    }
+          @Override
+          public void onNext(SearchResult searchResult) {
+            Timber.d(searchResult.toString());
+            if (searchResult.getItems().isEmpty()) {
+              Toast.makeText(activityContext, getString(R.string.no_result_found),
+                  Toast.LENGTH_SHORT).show();
+            }
+            adapter.setItems(searchResult.getItems());
+          }
+        });
+  }
 }
--- app/src/main/java/com/kallavistudios/samplesearch/data/DataManager.java
+++ app/src/main/java/com/kallavistudios/samplesearch/data/DataManager.java
@@ -1,16 +1,16 @@
 package com.kallavistudios.samplesearch.data;
 
-
+import android.support.annotation.NonNull;
 import com.kallavistudios.samplesearch.R;
 import com.kallavistudios.samplesearch.SampleSearchApplication;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -18,8 +18,10 @@ import rx.schedulers.Schedulers;
 
 public class DataManager {
 
+
     private final SampleSearchApplication application;
     private List<String> suggestions = new ArrayList<>();
+  private volatile Set<String> suggestionSet = new HashSet<>();
 
     public DataManager(SampleSearchApplication application) {
         this.application = application;
@@ -38,6 +40,61 @@ public class DataManager {
                     });
         }
         return suggestions;
+    }
+
+    public Observable<List<String>> getSuggestedWords(String query){
+        if(suggestionSet.isEmpty()){
+          return getSuggestedWordsFromDisk(query);
+        }else{
+          final List<String> result = new ArrayList<>();
+          for (String keyword : suggestionSet){
+            if(keyword.startsWith(query)){
+              result.add(keyword);
+            }
+          }
+          if(result.isEmpty()){
+            return getSuggestedWordsFromDisk(query)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io());
+          }else{
+            return Observable.just(result);
+          }
+        }
+    }
+
+  @NonNull
+  private Observable<List<String>> getSuggestedWordsFromDisk(String query) {
+    final Observable<List<String>> result = getSuggestionsMatching(query)
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeOn(Schedulers.io());
+    result.subscribe(new Action1<List<String>>() {
+      @Override
+      public void call(List<String> result) {
+        for(int i = 0 , size = result.size(); i < size; i++){
+          suggestionSet.add(result.get(i));
+        }
+      }
+    });
+    return result;
+  }
+
+  private Observable<List<String>> getSuggestionsMatching(String query){
+        List<String> result = new ArrayList<>();
+        try {
+            InputStream inputStream = application.getResources().openRawResource(R.raw.suggestions);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String keyword = reader.readLine();
+            while (keyword != null) {
+                if(keyword.startsWith(query)) {
+                    result.add(keyword);
+                }
+                keyword = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException ignore) {
+
+        }
+        return Observable.just(result);
     }
 
     private Observable<List<String>> populateSuggestionsTable() {
